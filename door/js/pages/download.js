88b0ad8812890e1d59a3b2966e540939f74419ea
yxy <yxy1993@99.com>
2018-12-04 09:56:42 +0800

    添加下载列表

---
+++ door/js/pages/download.js
@@ -0,0 +1,13 @@
+$(function () {
+
+    var products = $('.list-group').find('a');
+
+    products.click(function () {
+        //判断是否登录
+        // if() {
+        //     alert("请先登录")
+        // } else {
+        //
+        // }
+    })
+});
